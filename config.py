1d9f2f2076caefe5b4cf4ac23d717edf61e24a0c
stepan vinokurov <vinstp@yandex.ru>
2024-06-01 23:51:14 +0300

    change config

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ PATH_FOR_LOGS = './logs/'
 DEFAULT_CONFIRMATION_PERIOD = 2 * 60 if PROD_ENV else 5
 
 if PROD_ENV:
+    print("prod env secrets")
     with open("/run/secrets/downloader_bot_old_school_api_id", "r") as file:
         API_ID = file.read()
     with open("/run/secrets/downloader_bot_old_school_bot_token", "r") as file:
@@ -13,6 +14,7 @@ if PROD_ENV:
     with open("/run/secrets/downloader_bot_old_school_api_hash", "r") as file:
         API_HASH = file.read()
 else:
+    print("dev env secrets")
     import secrets
     API_ID = secrets.API_ID
     BOT_TOKEN = secrets.BOT_TOKEN
